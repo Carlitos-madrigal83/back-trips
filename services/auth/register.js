3fc83a9c59569280cf48497ce444e139ec370892
carlitos_madrigal <madrigalgallego.jc@gmail.com>
2022-09-15 16:21:47 +0200

    feat: added register service

--- services/auth/register.js
+++ services/auth/register.js
@@ -1,19 +1,18 @@
 const { createUser } = require("../../queries/auth");
-const { hash, mailer } = require("../../utils");
+const { hash } = require("../../utils");
 const { register } = require("../../errors/auth");
 const errors = require("../../errors/commons");
 
 module.exports = (db) => async (req, res, next) => {
-  const { email, password } = req.body;
+  const { email, password, username } = req.body;
 
   const queryResult = await createUser(db)({
     email,
     password: await hash.encrypt(password),
+    username,
   });
 
   if (!queryResult.ok) return next(register[queryResult.code] || errors[500]);
-
-  await mailer.send({ to: email, type: "welcome" });
 
   res.status(200).json({
     success: true,
