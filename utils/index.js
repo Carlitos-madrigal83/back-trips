d11695930353347bff7078f9fb416b0160b45b0e
carlitos_madrigal <madrigalgallego.jc@gmail.com>
2022-09-15 17:22:39 +0200

    feat: added utils

--- utils/index.js
+++ utils/index.js
@@ -1,5 +1,15 @@
 const hash = require("./hash");
+const jwt = require("./jwt");
+const cookie = require("./cookie");
+
+const serialize = (res, payload) => {
+    const token = jwt.sign(payload);
+    cookie.create(res, token);
+  };
 
 module.exports = {
     hash,
+    serialize,
+    jwt,
+    cookie,
 };
