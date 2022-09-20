485420ad8f21aa8781440ca0c22381562798bc7c
carlitos_madrigal <madrigalgallego.jc@gmail.com>
2022-09-20 17:50:29 +0200

    feat: init fly random

--- services/map-random/create.js
+++ services/map-random/create.js
@@ -1,25 +1,21 @@
-
+const randomCoordinates = require('random-coordinates');
 
 module.exports = (db) => async (req, res, next) => {
-  const { email, password } = req.body;
-
-  const queryResult = await getCorrectUser(db)({
-    email,
-    compareFn: hash.compare(password),
-  });
-
-  if (!queryResult.ok) return next(login[queryResult.code] || errors[500]);
-
-  serialize(res, { email: queryResult.data.email });
-
+  const saveCoordinates = randomCoordinates();
+  
   res.status(200).json({
     success: true,
+    data: saveCoordinates
   });
 };
 
 /*
     1º- Crear coordenadas al azar
     2º- Crear datos de vuelos al azar
+      * Iconos de compañias
+      * Hora de los vuelos
+      * Precio
+      * Duración del vuelo
     3º- Guardar coordenadas en la db
     4º- Devolver coordenadas y vuelos
 */ 
