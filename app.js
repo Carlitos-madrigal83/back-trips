2651a125d3507b54f4b26b30c7a2390437d3441e
carlitos_madrigal <madrigalgallego.jc@gmail.com>
2022-09-28 16:01:51 +0200

    feat: added cors

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
 require("dotenv").config();
 const express = require("express");
 const errors = require("./errors/commons");
+const options = require("./configs/cors");
+const cors = require("cors");
 
 const app = express();
 
 module.exports = async () => {
     const db = await require("./configs/db");
+
+    app.use(cors(options));
+
     app.use(express.json());
     
     app.use("/", require("./services")(db));
-    
+
     app.use((_, __, next) => {
         next(errors[404]);
     });
